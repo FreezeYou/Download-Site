a3176eb62f13aba39b9fd4d65cd4bd075d26984b
Playhi <000902play@gmail.com>
2021-12-22 22:37:25 +0800

    Initial commit

---
+++ sync.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import os
+import urllib.parse
+
+import requests
+import wget
+
+dataJson = requests.get("https://api.github.com/repos/FreezeYou/FreezeYou/releases").json()
+
+print("正在同步...")
+for releaseData in dataJson:
+    fileName: str = urllib.parse.unquote(releaseData["name"])
+    downloadUrl: str = urllib.parse.unquote(releaseData["assets"][0]["browser_download_url"])
+    if fileName is not None or downloadUrl is not None:
+        if not os.path.exists(fileName + ".apk"):
+            print("正在下载：" + downloadUrl)
+            wget.download(downloadUrl, fileName + ".apk")
+            print("\r\n同步 " + fileName + " 完成")
+        else:
+            print("已存在的：" + fileName + ".apk")
+    else:
+        print("\r\n同步 " + str(fileName) + " 失败")
+
+print("同步完成")
